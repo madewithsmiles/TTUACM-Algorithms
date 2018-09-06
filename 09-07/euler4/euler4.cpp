49fe0021e7cec1136ff283b99835811529ebddce
Urbana <ryan.kelley@ttu.edu>
2018-09-05 19:08:27 -0500

    Update euler4.cpp

--- 09-07/euler4/euler4.cpp
+++ 09-07/euler4/euler4.cpp
@@ -1,3 +1,32 @@
-int main() {
+#include <iostream>
+#include <string>
 
+int euler4(void);
+bool palindrome(std::string s);
+
+int main(void) {
+  std::cout << euler4() << std::endl;
+  return 0;
+}
+
+bool palindrome(std::string s) {
+  return s == std::string(s.rbegin(), s.rend());
+}
+
+int euler4(void) {
+  
+  int largest = 0;
+  
+  for (int i = 100; i <= 999; i++) {
+    for (int j = 100; j <= 999; j++) {
+      
+      int prod = i * j;
+      if (palindrome(std::to_string(prod))) {
+        if (prod > largest) {
+          largest = prod;
+        }
+      }
+    }
+  }
+  return largest;
 }
