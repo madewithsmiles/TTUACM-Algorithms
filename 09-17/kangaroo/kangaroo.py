3f960c94d7035f3d4b8457edc5ba017557762687
Urbana <ryan.kelley@ttu.edu>
2018-09-10 22:50:00 -0500

    Update kangaroo.py

--- 09-17/kangaroo/kangaroo.py
+++ 09-17/kangaroo/kangaroo.py
@@ -8,7 +8,7 @@ import sys
 
 '''
 Complete the kangaroo function below.
-The kangaroos will meet if there exists an integer z such that z = (x1 - x2) / (v2 - v1).
+The kangaroos will meet if there exists an integer j such that j = (x1 - x2) / (v2 - v1).
 This equation is derived from the following:
 We know that the position of any given kangaroo can be represented by the formula x = x0 + v0j where x is their current position,
 x0 is their initial position, v0 is their meters per jump, and j is the amount of jumps they have made
@@ -24,7 +24,7 @@ def kangaroo(x1, v1, x2, v2):
     posDiff = x1 - x2
     # If you divide by 0 then j is undefined
     if velDiff != 0:
-        # If z is a positive integer (no negative or partial jumps)
+        # If j is a positive integer (no negative or partial jumps)
         if posDiff % velDiff == 0 and posDiff / velDiff > 0:
             return 'YES'
     # Otherwise there is no j that satisfies our conditions
