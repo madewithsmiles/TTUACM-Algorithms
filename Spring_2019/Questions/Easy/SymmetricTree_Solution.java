fa37285b37ed8e6ff96f6595ca757f7f1d5309d7
ynigoreyes <ynigoreyes@gmail.com>
2019-03-26 12:44:24 -0500

    adding rajeev's solution

--- Spring_2019/Questions/Easy/SymmetricTree_Solution.java
+++ Spring_2019/Questions/Easy/SymmetricTree_Solution.java
@@ -17,17 +17,17 @@ class Solution {
     public boolean isSymmetric(TreeNode root) {
         return isSymmetric(root, root);
     }
-    
+
 
     /**
      * How this works...
-     * 
+     *
      * given: root of the left and right subtrees (nodes) of a given binary tree
-     * 
+     *
      * if either of the nodes are null, you have two choices:
      *      1) if their both null, you still have symmetry
      *      2) else, you don't have symmetry
-     * 
+     *
      * otherwise, you check three conditions to check,
      *      1) the value of both nodes are the same,
      *      2) the outer nodes of the two nodes are the same (left of left checked with right of right)
@@ -39,13 +39,13 @@ class Solution {
                 return true;
             return false;
         }
-        
+
         boolean checkAll = (leftCheck.val == rightCheck.val) &&
                            isSymmetric(leftCheck.left, rightCheck.right) &&
                            isSymmetric(leftCheck.right, rightCheck.left);
-        
+
         return checkAll;
-        
+
     }
-    
+
 }
